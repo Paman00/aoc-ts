f1f7af1a87393ea05c34f71556bd05ba3df69182
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-12-02 12:49:23 +0100

    Solved ex 1 from day 2 (2024)

---
+++ src/2024/day02-1.ts
@@ -0,0 +1,22 @@
+// Path: src/2024/day02
+import { openInput } from '../openInput.ts';
+
+const file = openInput(2024, 2);
+
+const lines = file.split('\n').slice(0, -1);
+
+const res = lines.map(isSafe).filter(Boolean).length;
+console.log(res);
+
+function isSafe(input: string): boolean {
+  const numbers: number[] = input.split(' ').map(Number);
+  // 1: increasing, -1: decreasing
+  const direction = numbers[1] > numbers[0] ? 1 : -1;
+  return numbers.every((n, i) => {
+    if (i == 0) return true;
+    const past = numbers[i - 1];
+    if (n == past) return false;
+    if (direction == 1 && past < n) return (n - past <= 3);
+    if (direction == -1 && past > n) return (past - n <= 3);
+  })
+}
