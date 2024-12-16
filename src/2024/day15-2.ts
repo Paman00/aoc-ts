4d465d2433bc7089bd309bf1deb978428dce9398
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-12-16 02:07:02 +0100

    Refactor ex 2 from day 15 (2024)

--- src/2024/day15-2.ts
+++ src/2024/day15-2.ts
@@ -4,7 +4,6 @@ import { openInput } from '../openInput.ts';
 const file = openInput(2024, 15);
 const [initialMap, moves] = parseData(file);
 const map = simulateMoves(initialMap, moves);
-console.log(map.join('\n'));
 const result = calculateGPS(map);
 console.log(result);
 
@@ -34,7 +33,6 @@ function parseData(data: string): [string[], Direction[]] {
 }
 
 function simulateMoves(map: string[], moves: Direction[]): string[] {
-  // TODO: refactor
   const directions = {
     [Direction.top]: { x: 0, y: -1 },
     [Direction.bottom]: { x: 0, y: 1 },
@@ -43,74 +41,73 @@ function simulateMoves(map: string[], moves: Direction[]): string[] {
   };
 
   const canMoveBox = (dir: Direction, [y, x]: number[]): boolean => {
-    const {/*x: dx,*/y: dy } = directions[dir];
+    const { x: dx, y: dy } = directions[dir];
+    const nextX1 = x + dx;
+    const nextY = y + dy;
     if (dir === Direction.top || dir === Direction.bottom) {
-      const nextY = y + dy;
-      const nextX1 = x;
       const nextX2 = x + 1;
-      if (!isInLimits(map, nextX1, nextY) || !isInLimits(map, nextX2, nextY) || map[nextY][nextX1] === '#' || map[nextY][nextX2] === '#') {
+      if (!isInLimits(map, nextX1, nextY) || map[nextY][nextX1] === '#') {
         return false;
       }
-      if ((map[nextY][nextX1] === '[' || map[nextY][nextX1] === ']') && !canMoveBox(dir, [nextY, map[nextY][nextX1] === '[' ? nextX1 : nextX1 - 1])) {
+      if (!isInLimits(map, nextX2, nextY) || map[nextY][nextX2] === '#') {
         return false;
       }
-      if ((map[nextY][nextX2] === '[' || map[nextY][nextX2] === ']') && !canMoveBox(dir, [nextY, map[nextY][nextX2] === '[' ? nextX2 : nextX2 - 1])) {
+      const nextChar1 = map[nextY][nextX1];
+      const nextChar2 = map[nextY][nextX2];
+      if (nextChar1 === '[' || nextChar1 === ']') {
+        const xStartBox = nextChar1 === '[' ? nextX1 : nextX1 - 1;
+        if (!canMoveBox(dir, [nextY, xStartBox])) return false;
+      }
+      if (nextChar2 === '[' || nextChar2 === ']') {
+        const xStartBox = nextChar2 === '[' ? nextX2 : nextX2 - 1;
+        if (!canMoveBox(dir, [nextY, xStartBox])) return false;
+      }
+    } else {
+      const nextX2 = x + 2 * dx;
+      const nextChar1 = map[nextY][nextX1];
+      const nextChar2 = map[nextY][nextX2];
+      if (!isInLimits(map, nextX1, nextY) || nextChar1 === '#') {
         return false;
       }
-      return true;
+      if (
+        dir === Direction.right &&
+        (!isInLimits(map, nextX2, nextY) || nextChar2 === '#')
+      ) {
+        return false;
+      }
+      if (dir === Direction.right && nextChar2 === '[') {
+        return canMoveBox(dir, [nextY, nextX2]);
+      } else if (dir === Direction.left && nextChar1 === ']') {
+        return canMoveBox(dir, [nextY, nextX2]);
+      }
     }
     return true;
   };
 
-  const moveBox = (dir: Direction, [y, x]: number[]): boolean => {
+  const moveBox = (dir: Direction, [y, x]: number[]): void => {
     const { x: dx, y: dy } = directions[dir];
+    const nextY = y + dy;
+    const nextX1 = x + dx;
     if (dir === Direction.top || dir === Direction.bottom) {
-      const nextY = y + dy;
-      const nextX1 = x;
       const nextX2 = x + 1;
-      if (!isInLimits(map, nextX1, nextY) || !isInLimits(map, nextX2, nextY) || map[nextY][nextX1] === '#' || map[nextY][nextX2] === '#') {
-        return false;
+      if (map[nextY][nextX1] === '[' || map[nextY][nextX1] === ']') {
+        const xstartBox = map[nextY][nextX1] === '[' ? nextX1 : nextX1 - 1;
+        moveBox(dir, [nextY, xstartBox]);
       }
-      if ((map[nextY][nextX1] === '[' || map[nextY][nextX1] === ']') && !canMoveBox(dir, [nextY, map[nextY][nextX1] === '[' ? nextX1 : nextX1 - 1])) {
-        return false;
+      if (map[nextY][nextX2] === '[' || map[nextY][nextX2] === ']') {
+        const xstartBox = map[nextY][nextX2] === '[' ? nextX2 : nextX2 - 1;
+        moveBox(dir, [nextY, xstartBox]);
       }
-      if ((map[nextY][nextX2] === '[' || map[nextY][nextX2] === ']') && !canMoveBox(dir, [nextY, map[nextY][nextX2] === '[' ? nextX2 : nextX2 - 1])) {
-        return false;
-      }
-      if ((map[nextY][nextX1] === '[' || map[nextY][nextX1] === ']') && !moveBox(dir, [nextY, map[nextY][nextX1] === '[' ? nextX1 : nextX1 - 1])) {
-        return false;
-      }
-      if ((map[nextY][nextX2] === '[' || map[nextY][nextX2] === ']') && !moveBox(dir, [nextY, map[nextY][nextX2] === '[' ? nextX2 : nextX2 - 1])) {
-        return false;
-      }
-      map[y] = map[y].slice(0, x) + '..' + map[y].slice(x + 2);
-      map[nextY] = map[nextY].slice(0, nextX1) + '[]' + map[nextY].slice(nextX1 + 2);
-      return true;
-    }
-    const nextY = y;
-    if (dir === Direction.right) {
-      const nextX1 = x + dx;
+      updateItem(map, [y, x], [nextY, nextX1], '[]');
+    } else {
       const nextX2 = x + 2 * dx;
-      if (!isInLimits(map, nextX1, y) || !isInLimits(map, nextX2, y) || map[y][nextX1] === '#' || map[y][nextX2] === '#') {
-        return false;
+      if (dir === Direction.right && map[nextY][nextX2] === '[') {
+        moveBox(dir, [y, nextX2]);
+      } else if (dir === Direction.left && map[nextY][nextX1] === ']') {
+        moveBox(dir, [y, nextX2]);
       }
-      if (map[y][nextX2] === '[' && !moveBox(dir, [y, nextX2])) {
-        return false;
-      }
-      map[y] = map[y].slice(0, x) + '..' + map[y].slice(x + 2);
-      map[y] = map[y].slice(0, nextX1) + '[]' + map[y].slice(nextX1 + 2);
-      return true;
+      updateItem(map, [y, x], [y, nextX1], '[]');
     }
-    const nextX = x + dx;
-    if (!isInLimits(map, nextX, nextY) || map[nextY][nextX] === '#') {
-      return false;
-    }
-    if (map[nextY][nextX] === ']' && !moveBox(dir, [nextY, nextX - 1])) {
-      return false;
-    }
-    map[y] = map[y].slice(0, x) + '..' + map[y].slice(x + 2);
-    map[nextY] = map[nextY].slice(0, nextX) + '[]' + map[nextY].slice(nextX + 2);
-    return true;
   };
 
   const moveRobot = (dir: Direction, [y, x]: number[]): number[] => {
@@ -121,11 +118,14 @@ function simulateMoves(map: string[], moves: Direction[]): string[] {
     if (!isInLimits(map, nextX, nextY) || nextChar === '#') {
       return [y, x];
     }
-    if ((nextChar === '[' || nextChar === ']') && !moveBox(dir, [nextY, nextChar === '[' ? nextX : nextX - 1])) {
-      return [y, x];
+    if (nextChar === '[' || nextChar === ']') {
+      const xStartBox = nextChar === '[' ? nextX : nextX - 1;
+      if (!canMoveBox(dir, [nextY, xStartBox])) {
+        return [y, x];
+      }
+      moveBox(dir, [nextY, xStartBox]);
     }
-    map[y] = map[y].slice(0, x) + '.' + map[y].slice(x + 1);
-    map[nextY] = map[nextY].slice(0, nextX) + '@' + map[nextY].slice(nextX + 1);
+    updateItem(map, [y, x], [nextY, nextX], '@');
     return [nextY, nextX];
   };
 
@@ -137,10 +137,6 @@ function simulateMoves(map: string[], moves: Direction[]): string[] {
   return map;
 }
 
-function isInLimits(map: string[], x: number, y: number): boolean {
-  return y >= 0 && y < map.length && x >= 0 && x < map[0].length;
-}
-
 function findRobot(map: string[]): [number, number] {
   for (let i = 1; i < map.length - 1; i++) {
     const x = map[i].indexOf('@');
@@ -148,7 +144,25 @@ function findRobot(map: string[]): [number, number] {
       return [i, x];
     }
   }
-  return [1, 1];
+  return [1, 2];
+}
+
+function isInLimits(map: string[], x: number, y: number): boolean {
+  return y >= 0 && y < map.length && x >= 0 && x < map[0].length;
+}
+
+function updateItem(
+  map: string[],
+  pastC: number[],
+  newC: number[],
+  item: string,
+): void {
+  const [y, x] = pastC;
+  const [newY, newX] = newC;
+  map[y] =
+    `${map[y].slice(0, x)}${'.'.repeat(item.length)}${map[y].slice(x + item.length)}`;
+  map[newY] =
+    `${map[newY].slice(0, newX)}${item}${map[newY].slice(newX + item.length)}`;
 }
 
 function calculateGPS(map: string[]): number {
@@ -162,6 +176,7 @@ function calculateGPS(map: string[]): number {
   }
   return result;
 }
+
 enum Direction {
   top = '^',
   right = '>',
